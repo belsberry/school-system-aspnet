74a57785f486900b200a34c961eee6c1c0717ee5
belsberry <ben.elsberry@gmail.com>
2015-12-01 14:01:29 -0600

    Add code coverage

--- SchoolInformationSystem.Web/karma.conf.js
+++ SchoolInformationSystem.Web/karma.conf.js
@@ -1,6 +1,31 @@
 var gulpConfig = require("./gulp.config");
 
 module.exports = function(config) {
+  var bowerFiles = [
+    //bower:js
+    "wwwroot/.tmp/bower_components/angular/angular.js",
+    "wwwroot/.tmp/bower_components/angular-route/angular-route.js",
+    "wwwroot/.tmp/bower_components/Chart.js/Chart.js",
+    "wwwroot/.tmp/bower_components/angular-chart.js/dist/angular-chart.js",
+    "wwwroot/.tmp/bower_components/underscore/underscore.js",
+    "wwwroot/.tmp/bower_components/angular-toastr/dist/angular-toastr.tpls.js",
+    "wwwroot/.tmp/bower_components/angular-bootstrap/ui-bootstrap-tpls.js",
+    "wwwroot/.tmp/bower_components/angular-mocks/angular-mocks.js",
+    //endbower
+  ]
+  var coverageFileList = [
+      gulpConfig.tempAppFolder.slice(2) + "**/*.js",
+      gulpConfig.root.slice(2) + gulpConfig.templateCache.outFile,
+      gulpConfig.jasmine.specs.slice(2)
+  ];
+  var fullFileList = [].concat(bowerFiles, coverageFileList);
+  function getCoverageList(){
+    var coverageObj = {};
+    for(var i = 0; i < coverageFileList.length; i++){
+      coverageObj[coverageFileList[i]] = "coverage";
+    }
+    return coverageObj;
+  }
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -13,21 +38,7 @@ module.exports = function(config) {
 
 
     // list of files / patterns to load in the browser
-    files: [
-      //bower:js
-      "wwwroot/.tmp/bower_components/angular/angular.js",
-      "wwwroot/.tmp/bower_components/angular-route/angular-route.js",
-      "wwwroot/.tmp/bower_components/Chart.js/Chart.js",
-      "wwwroot/.tmp/bower_components/angular-chart.js/dist/angular-chart.js",
-      "wwwroot/.tmp/bower_components/underscore/underscore.js",
-      "wwwroot/.tmp/bower_components/angular-toastr/dist/angular-toastr.tpls.js",
-      "wwwroot/.tmp/bower_components/angular-bootstrap/ui-bootstrap-tpls.js",
-      "wwwroot/.tmp/bower_components/angular-mocks/angular-mocks.js",
-      //endbower
-      gulpConfig.tempAppFolder.slice(2) + "**/*.js",
-      gulpConfig.templateCache.outFile.slice(2),
-      gulpConfig.jasmine.specs.slice(2)
-    ],
+    files: fullFileList,
 
 
     // list of files to exclude
@@ -37,14 +48,13 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+    preprocessors: getCoverageList(),
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
     // web server port
     port: 9876,
@@ -70,6 +80,12 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+    
+    coverageReporter: {
+      type : 'text'
+    }    
+    
+    
   })
 }
